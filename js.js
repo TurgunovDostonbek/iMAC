6bde1cb20b719bfa22d118bf1776c0220d218a5b
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2024-01-23 01:05:27 +0500

    navbar_____

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector(".menu");
 const navbarModal = document.querySelector(".navbar_modal");
 const close1 = document.querySelector(".close1");
+const bxMoon = document.querySelector("#bx-moon");
+const bxSun = document.querySelector(".bx-sun");
 
 menu.addEventListener("click", ()=>{
     navbarModal.style.transform = "translateY(0%)"
@@ -8,4 +10,9 @@ menu.addEventListener("click", ()=>{
 
 close1.addEventListener("click", ()=>{
     navbarModal.style.transform = "translateY(-100%)"
+})
+
+bxMoon.addEventListener('Onclick', () =>{
+    bxMoon.classList.toggle("bxSun")
+    
 })
